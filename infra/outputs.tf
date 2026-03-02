20996c29ee6904bc07ae52c1257e4a24cd84e0d9
vinicius <viniciuscaiquesilva@gmail.com>
2026-03-02 00:02:53 -0300

    add s3 back for network

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,11 @@ output "environment" {
 output "db_connection_string" {
   value = local.public ? "Server=${aws_db_instance.database.address},${aws_db_instance.database.port};Database=fiap-mechanics;User Id=${random_string.database_user.result};Password=${random_string.database_password.result};TrustServerCertificate=True;" : null
 }
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
