5f93443e306971262cb803abdb1995c8c4f26409
Augusto César Bisognin <augustocb@gmail.com>
2026-03-04 01:11:14 -0300

    validar certificado ao conectar

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -5,6 +5,6 @@ resource "aws_secretsmanager_secret" "db_credentials" {
 resource "aws_secretsmanager_secret_version" "db_credentials_value" {
   secret_id = aws_secretsmanager_secret.db_credentials.id
   secret_string = jsonencode({
-    value = "Server=${aws_db_instance.database.address},${aws_db_instance.database.port};Database=fiap-mechanics;User Id=${random_string.database_user.result};Password=${random_password.database_password.result};TrustServerCertificate=True;"
+    value = "Server=${aws_db_instance.database.address},${aws_db_instance.database.port};Database=fiap-mechanics;User Id=${random_string.database_user.result};Password=${random_password.database_password.result};"
   })
 }
